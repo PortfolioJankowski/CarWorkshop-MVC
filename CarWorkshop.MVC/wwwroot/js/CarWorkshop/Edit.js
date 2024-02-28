d680e33bb07b94d1a14ea6e13aac4810b785a7e1
PortfolioJankowski <mat.jankowski99@gmail.com>
2024-02-28 09:09:06 +0100

    Refactor of JS sript (LoadCarWorkshopServices())

--- CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
@@ -2,39 +2,6 @@
 //je¿eli html zosta³ za³adowany to wykona siê funkcja wewn¹trz
 $(document).ready(function () {
 
-    const RenderCarWorkshopServices = (services, container) => {
-        container.empty();
-
-        for (const service of services) {
-            container.append(`
-            <div class="card borded-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">${service.cost}</div>
-                <div class="card-body">
-                    <h5 class="card-title">${service.description}</h5>
-                </div>
-            </div>`)
-        }
-    }
-
-    const LoadCarWorkshopServices = () => {
-        const container = $("#services")
-        const carWorkshopEncodedName = container.data("encodedName");
-
-        $.ajax({
-            url: `/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService`,
-            type: 'get',
-            success: function (data) {
-                if (!data.length) {
-                    container.html("There are no services for this car workshop")
-                } else {
-                    RenderCarWorkshopServices(data, container)
-                }
-            },
-            error: function () {
-                toastr["error"]("Something went wrong")
-            }
-        })
-    }
 
     LoadCarWorkshopServices()
     //gdy formularz zostanie przesk³any (id formularza to createCarServiceModal) to funkcja zostanie wywo³ana
@@ -48,6 +15,7 @@ $(document).ready(function () {
             data: $(this).serialize(),
             success: function (data) {
                 toastr["success"]("Created carworkshop service")
+                LoadCarWorkshopServices()
             },
             error: function () {
                 toastr["error"]("Something went wrong")
