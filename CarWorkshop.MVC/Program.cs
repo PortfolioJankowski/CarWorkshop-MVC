a80d2ef31e67575012d9a5f548e365a706d3cca5
Mateusz <143873536+PortfolioJankowski@users.noreply.github.com>
2025-01-12 11:05:28 +0100

    Update Program.cs

--- CarWorkshop.MVC/Program.cs
+++ CarWorkshop.MVC/Program.cs
@@ -10,22 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 // dependency injection - konfiguracja kontenera
 builder.Services.AddControllersWithViews(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
 
-builder.Services.AddInfrastructure(builder.Configuration)
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 //buduje z serwisami
 var app = builder.Build();
 
-//tworzê zakres us³ug dla aplikacji 
+//tworzÃª zakres usÂ³ug dla aplikacji 
 var scope = app.Services.CreateScope();
-//pobieram konkretn¹ us³ugê z tego zakresu us³ug -> Seedera doda³em
+//pobieram konkretnÂ¹ usÂ³ugÃª z tego zakresu usÂ³ug -> Seedera dodaÂ³em
 //w AddInfrastructure poprzez services.AddScoped<Seeder>()
 var seeder = scope.ServiceProvider.GetRequiredService<CarWorkshopSeeder>();
-//wywo³uje metodê, która sprawdzi czy baza danych ma recordy a jak nie ma to doda przypadkowy
+//wywoÂ³uje metodÃª, ktÃ³ra sprawdzi czy baza danych ma recordy a jak nie ma to doda przypadkowy
 await seeder.Seed();
 
-//tutaj info jak aplikacja ma siê zachowywaæ
-// sprawdzenie czy aktualnie jesteœmy w œrodowisku develpment
+//tutaj info jak aplikacja ma siÃª zachowywaÃ¦
+// sprawdzenie czy aktualnie jesteÅ“my w Å“rodowisku develpment
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -33,24 +33,24 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-//przekierowywanie na https -> przekierowanie zapytañ nieu¿ywaj¹cych tego protoko³u
+//przekierowywanie na https -> przekierowanie zapytaÃ± nieuÂ¿ywajÂ¹cych tego protokoÂ³u
 app.UseHttpsRedirection();
 
-//je¿eli klient bedzie siê odnosi³ do plików np. css czy js to bêdziemy je zwracaæ do klienta
+//jeÂ¿eli klient bedzie siÃª odnosiÂ³ do plikÃ³w np. css czy js to bÃªdziemy je zwracaÃ¦ do klienta
 app.UseStaticFiles();
 
 
-// dopasowywanie œcie¿ek do konkretnych handlarów
+// dopasowywanie Å“cieÂ¿ek do konkretnych handlarÃ³w
 app.UseRouting();
 
 app.UseAuthorization();
 
-//pattern w jaki sposób bedziemy dopasowywaæ œcie¿kê do handlerów
+//pattern w jaki sposÃ³b bedziemy dopasowywaÃ¦ Å“cieÂ¿kÃª do handlerÃ³w
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//Login korzysta z RazorPages, wiêc trzeba sobie to zmapowaæ
+//Login korzysta z RazorPages, wiÃªc trzeba sobie to zmapowaÃ¦
 app.MapRazorPages();
-// jak to dzia³a? Home to folder w Views, i ma plik Index :D wiêc tak aplikacja szuka plików
+// jak to dziaÂ³a? Home to folder w Views, i ma plik Index :D wiÃªc tak aplikacja szuka plikÃ³w
 app.Run();
